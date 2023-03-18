7ced5f13b163b064feeca197f3077a1d7bb0dbbc
Henry <henhen1227@gmail.com>
2023-03-17 23:47:23 -0400

    Changed database path back

--- src/index.js
+++ src/index.js
@@ -32,19 +32,19 @@ app.use(cors(corsOptions));
 app.use(express.json());
 //MARK: APP STORE API
 
-// let appStoreApi = new AppStoreConnectApi();
-// appStoreApi.reloadApi().then();
-// let lastAppStoreUpdate = Date.now();
-// updateAppStore();
-// setInterval(updateAppStore, 4  * 60 * 60 * 1000);
-// function updateAppStore(){
-//     appStoreApi.reloadApi().then();
-//
-//     const today = new Date();
-//     lastAppStoreUpdate = String(today.getMonth()+1) + '-' + String(today.getDate()) + '-' + today.getFullYear() + ' ' + String(today.getHours()) + ":" + String(today.getMinutes()) + ":" + String(today.getSeconds()).padStart(2, '0');
-//
-//     console.log(lastAppStoreUpdate);
-// }
+let appStoreApi = new AppStoreConnectApi();
+appStoreApi.reloadApi().then();
+let lastAppStoreUpdate = Date.now();
+updateAppStore();
+setInterval(updateAppStore, 4  * 60 * 60 * 1000);
+function updateAppStore(){
+    appStoreApi.reloadApi().then();
+
+    const today = new Date();
+    lastAppStoreUpdate = String(today.getMonth()+1) + '-' + String(today.getDate()) + '-' + today.getFullYear() + ' ' + String(today.getHours()) + ":" + String(today.getMinutes()) + ":" + String(today.getSeconds()).padStart(2, '0');
+
+    console.log(lastAppStoreUpdate);
+}
 
 app.get('/appstore/apps', (req, res) => {
     res.send({
