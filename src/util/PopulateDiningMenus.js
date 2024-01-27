be6c9ba459393cda9944fa4eccca93132402f449
Henry <henhen1227@gmail.com>
2024-01-27 11:55:45 -0500

    Added Grab-n-go menu to SPS Now Dining API

--- src/util/PopulateDiningMenus.js
+++ src/util/PopulateDiningMenus.js
@@ -4,3 +4,13 @@ import {scrapeAllMenus} from "../calendar/Dining.js";
 scrapeAllMenus().then(() => {
     console.log('Updated all menus!');
 });
+
+// Bash script to copy the file at /Users/henryabrahamsen/WebstormProjects/henhen1227-api/database/dining/breakfastMenu.json
+// to the server at henry@api.henhen1227.com:/home/henry/Projects/henhen1227-api/database/dining
+/*
+
+    scp /Users/henryabrahamsen/WebstormProjects/henhen1227-api/database/dining/breakfastMenu.json henry@api.henhen1227.com:/home/henry/Projects/henhen1227-api/database/dining \
+    scp /Users/henryabrahamsen/WebstormProjects/henhen1227-api/database/dining/brunchMenu.json henry@api.henhen1227.com:/home/henry/Projects/henhen1227-api/database/dining \
+    scp /Users/henryabrahamsen/WebstormProjects/henhen1227-api/database/dining/lunchMenu.json henry@api.henhen1227.com:/home/henry/Projects/henhen1227-api/database/dining \
+    scp /Users/henryabrahamsen/WebstormProjects/henhen1227-api/database/dining/dinnerMenu.json henry@api.henhen1227.com:/home/henry/Projects/henhen1227-api/database/dining
+*/
